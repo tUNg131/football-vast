57df747225cc740a79434d88d6bb1bdacd7f878d
Tung Xuan Le <halethuy.hlt@gmail.com>
2024-03-08 04:37:00 +0000

    edit command

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 # Update package list and install Git
-# pip install h5py lightning tensorboard
+# pip install h5py lightning tensorboard gdown
 
-python /workspace/main.py \
-    --train-path ./data/train.hdf5 \
-    --val-path ./data/val.hdf5 \
-    --batch-size=1 --version test \
+# Download data
+# gdown 1BsysF9kXg0WWWQ7RYufecsiOGLyZRxbD --folder --output /workspace/data
+
+# git clone
+# git clone https://github.com/tUNg131/football-vast.git /workspace/src
+
+python /workspace/src/main.py train \
+    --train-path /workspace/data/train.hdf5 \
+    --val-path /workspace/data/val.hdf5 \
+    --batch-size 1 \
+    --version test \
     --max-epochs 1 \
     --fast-dev-run \
     --precision transformer-engine
